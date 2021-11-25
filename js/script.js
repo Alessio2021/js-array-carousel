cdb6215726833878f6681b42ba2eb6bd9eaa2540
Alessio2021 <alessiobut1911@gmail.com>
2021-11-25 17:34:15 +0100

    click event

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const items = [
     'img/03.jpg',
     'img/04.jpg',
     'img/05.jpg'
-];
+]
 
 const title = [
     'Svezia',
@@ -19,15 +19,32 @@ const text = [
     'Lorem ipsum',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
 ]
 
 const slider = document.querySelector(".slider");
 const mainImg = document.querySelector(".main-img");
 
+// inserisco foto in html
 for (let index = 0; index < items.length; index++) {
     let image = `<img src="${items[index]}" alt="">`
     // console.log(items[index]);
-
     mainImg.innerHTML += image;
+}
+
+// assegno classe active per display block
+const img = document.querySelectorAll("img");
+img[0].classList.add("active");
+
+const arrowUp = document.querySelector(".arrow-up");
+const arrowDown = document.querySelector(".arrow-down");
+// assegno al bottone il cambio foto
+
+arrowUp.addEventListener('click', function () {
+    const activeImg = document.querySelector(".active");
+    // rimuovo l active e lo riassegno alla foto successiva
+    const next = activeImg.nextElementSibling;
+    activeImg.classList.remove("active");
+    activeImg.classList.add("active");
+
 }
